b5fec1e4743bbb1b29e2289f4f14224b4c2f2616
Thamalpath <BixSoftOfficial.com>
2023-06-16 18:13:13 +0530

    Permissions & Roles

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,34 @@
 <!-- need to remove -->
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+    <a href="{{ route('dashboard') }}" class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
 </li>
+{{-- @can('rooms') --}}
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-bed"></i>
+        <p>
+            Example
+            <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview" style="display: none;">
+        <li class="nav-item">
+            {{-- <a href="{{route('room.index')}}" class="nav-link  {{ Request::is('room.index') ? 'active' : '' }}"> --}}
+            <a href="#" class="nav-link">
+                <i class="fas fa-list nav-icon"></i>
+                <p>Example 1</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            {{-- <a href="{{route('room.create')}}" class="nav-link  {{ Request::is('room.create') ? 'active' : '' }}"> --}}
+            <a href="#" class="nav-link">
+                <i class="fas fa-folder-plus nav-icon"></i>
+                <p>Example 2</p>
+            </a>
+        </li>
+    </ul>
+</li>
+{{-- @endcan --}}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,7 +34,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
