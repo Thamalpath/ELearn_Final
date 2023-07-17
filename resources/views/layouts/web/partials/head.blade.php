cb36cc3a127da08673b3a1e5250624200bc8b678
Thamalpath <BixSoftOfficial.com>
2023-07-17 18:23:50 +0530

    Added Asset

--- resources/views/layouts/web/partials/head.blade.php
+++ resources/views/layouts/web/partials/head.blade.php
@@ -1,23 +1,22 @@
+<meta charset="UTF-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge" />
+<meta name="robots" content="index, follow" />
+<title>Jesco</title>
+<meta name="description" content="Jesco - Fashoin eCommerce HTML Template" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <meta charset="UTF-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="robots" content="index, follow" />
-    <title>Jesco</title>
-    <meta name="description" content="Jesco - Fashoin eCommerce HTML Template" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<!-- Add site Favicon -->
+<link rel="shortcut icon" href="{{ asset('images/favicon/favicon.ico" type="image/png') }}">
 
-    <!-- Add site Favicon -->
-    <link rel="shortcut icon" href="images/favicon/favicon.ico" type="image/png">
+<!-- vendor css (Icon Font) -->
+<link rel="stylesheet" href="{{ asset('css/vendor/bootstrap.bundle.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/vendor/pe-icon-7-stroke.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/vendor/font.awesome.css') }}" />
 
-    <!-- vendor css (Icon Font) -->
-    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap.bundle.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/vendor/pe-icon-7-stroke.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/vendor/font.awesome.css') }}" />
-
-    <!-- plugins css (All Plugins Files) -->
-    <link rel="stylesheet" href="{{ asset('css/plugins/animate.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/plugins/swiper-bundle.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/plugins/jquery-ui.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/plugins/nice-select.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/plugins/venobox.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+<!-- plugins css (All Plugins Files) -->
+<link rel="stylesheet" href="{{ asset('css/plugins/animate.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/plugins/swiper-bundle.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/plugins/jquery-ui.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/plugins/nice-select.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/plugins/venobox.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/style.css') }}" />
