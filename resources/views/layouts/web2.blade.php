f2ecbce84eb44d8616debbc30e1549ce4db18d5b
Thamalpath <BixSoftOfficial.com>
2023-07-19 18:35:01 +0530

    Added Web2 & Product List View

---
+++ resources/views/layouts/web2.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    @include('layouts.web.partials.head')
+    @stack('css')
+</head>
+
+<body>
+
+    <!-- Top Bar -->
+    <!-- Header Area Start -->
+    <!-- OffCanvas Menu Start -->
+    @include('layouts.web.partials.header')
+    <!-- Header Area End -->
+    <!-- OffCanvas Menu End -->
+    <div class="offcanvas-overlay"></div>
+
+    @yield('pageTitle')
+    @yield('content')
+
+    <!-- Footer Area Start -->
+    @include('layouts.web.partials.footer')
+    <!-- Footer Area End -->
+
+    <!-- Search Modal -->
+    <x-home.search-modal />
+
+    <!-- Login Modal -->
+    <x-home.login-modal />
+
+    <!-- Product Modal -->
+    <x-home.product-modal />
+
+    <!-- medium-zoom JS -->
+
+    <!-- Vendor JS -->
+    <script src="{{ asset('js/vendor/modernizr-3.11.2.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/jquery-migrate-3.3.2.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/countdown.js') }}"></script>
+    <script src="{{ asset('js/plugins/scrollup.js') }}"></script>
+    <script src="{{ asset('js/plugins/jquery.zoom.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/venobox.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/ajax-mail.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    @stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/web/products/list.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.web2')
+@section('pageTitle')
+    <!-- breadcrumb-area start -->
+    <div class="breadcrumb-area">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-12 text-center">
+                    <h2 class="breadcrumb-title">Shop</h2>
+                    <!-- breadcrumb-list start -->
+                    <ul class="breadcrumb-list">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item active">Shop</li>
+                    </ul>
+                    <!-- breadcrumb-list end -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb-area end -->
+@endsection
+
+@section('content')
+    <!-- Shop Page Start  -->
+    <div class="shop-category-area pt-100px pb-100px">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 order-lg-last col-md-12 order-md-first">
+                    <!-- Shop Top Area Start -->
+                    <x-product.shop-top />
+                    <!-- Shop Top Area End -->
+
+                    <!-- Shop Bottom Area Start -->
+                    <div class="shop-bottom-area">
+
+                        <!-- Tab Content Area Start -->
+                        <div class="row">
+                            <div class="col">
+                                <div class="tab-content">
+                                    <div class="tab-pane fade show active" id="shop-grid">
+                                        <div class="row mb-n-30px">
+                                            @foreach ($allProducts as $product)
+                                                <x-product.list-item :product="$product" />
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Tab Content Area End -->
+                    </div>
+                    <!-- Shop Bottom Area End -->
+
+                </div>
+
+                <!-- Sidebar Area Start -->
+                <x-product.shop-sidebar />
+                <!-- Sidebar Area End -->
+            </div>
+        </div>
+    </div>
+    <!-- Shop Page End  -->
+@endsection
