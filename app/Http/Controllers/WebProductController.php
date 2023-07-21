0103abb1316d47b796ac9c54f5d41cbc5cd1bd89
Thamalpath <BixSoftOfficial.com>
2023-07-21 23:55:48 +0530

    Show Related Products by Sub_Cat_Name

--- app/Http/Controllers/WebProductController.php
+++ app/Http/Controllers/WebProductController.php
@@ -20,12 +20,18 @@ class WebProductController extends Controller
     {
         $product = Product::where('slug', $slug)->first();
         $categories = Category::with('subCategories')->get();
+
+        // Fetch related products by filtering products with the same subcategory name
+        $relatedProducts = Product::where('sub_category_name', $product->sub_category_name)
+        ->where('id', '<>', $product->id)
+        ->limit(6)
+        ->get();
         
         // Convert the comma-separated colors and sizes stored in the database to arrays
         $productColors = explode(',', $product->color);
         $productSizes = explode(',', $product->size);
 
-        return view('web.products.show', compact('product', 'categories', 'productColors', 'productSizes'));
+        return view('web.products.show', compact('product', 'categories', 'productColors', 'productSizes', 'relatedProducts'));
     }
 
 }
