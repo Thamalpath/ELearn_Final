9a8714c21fb2083bca5611dcac4aac8529dedf5a
Thamalpath <BixSoftOfficial.com>
2023-07-21 21:00:09 +0530

    Added CSRF Token, Title,Notify Link

--- resources/views/layouts/web/partials/head.blade.php
+++ resources/views/layouts/web/partials/head.blade.php
@@ -1,7 +1,12 @@
 <meta charset="UTF-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge" />
 <meta name="robots" content="index, follow" />
-<title>Jesco</title>
+
+{{-- CSRF Token --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>@yield('title')</title>
+
 <meta name="description" content="Jesco - Fashoin eCommerce HTML Template" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
@@ -21,3 +26,5 @@
 <link rel="stylesheet" href="{{ asset('css/plugins/venobox.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 <link rel="stylesheet" href="{{ asset('css/dash/custom.css') }}" />
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
