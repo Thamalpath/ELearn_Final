a9d908214568e1d94e0095bf3e0833161bc3d1ae
Thamalpath <BixSoftOfficial.com>
2023-07-21 17:04:00 +0530

    Modified Name as Carts

--- database/migrations/2023_07_21_072551_create_cars_table.php
+++ database/migrations/2023_07_21_072551_create_cars_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cars', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
             $table->string('prod_id');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cars');
+        Schema::dropIfExists('carts');
     }
 };
