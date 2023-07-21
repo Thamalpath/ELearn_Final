a77d9ccb8cdacbf008687c137908a328482418e0
Thamalpath <BixSoftOfficial.com>
2023-07-21 21:01:31 +0530

    Revert "Added CSRF Token, Title, Notyf Link"
    This reverts commit 8f4a3706bdd505229c4c074bcfef43607d42ccf8.

--- resources/views/layouts/web/partials/header.blade.php
+++ resources/views/layouts/web/partials/header.blade.php
@@ -17,24 +17,20 @@
                                     <li class="d-flex">
                                         <ul class="d-block">
                                             <div class="image-wrapper mt-4">
-                                                <img src="{{ asset('images/banner/Shop_Image.jpg') }}" alt="Shop Image"
-                                                    width="250px" height="auto" class="shop-image">
-                                                @foreach ($categories as $category)
-                                                    @foreach ($category->subCategories as $subCategory)
-                                                        <img src="{{ asset('storage/' . $subCategory->image) }}"
-                                                            alt="{{ $subCategory->name }} Image" width="250px"
-                                                            height="auto" class="sub-category-image">
+                                                <img src="{{ asset('images/banner/Shop_Image.jpg') }}" alt="Shop Image" width="250px" height="auto" class="shop-image">
+                                                @foreach($categories as $category)
+                                                    @foreach($category->subCategories as $subCategory)
+                                                        <img src="{{ asset('storage/' . $subCategory->image) }}" alt="{{ $subCategory->name }} Image" width="250px" height="auto" class="sub-category-image">
                                                     @endforeach
                                                 @endforeach
                                             </div>
                                         </ul>
-                                        @foreach ($categories as $category)
+                                        @foreach($categories as $category)
                                             <ul class="d-block">
                                                 <li class="title"><a href="#">{{ $category->name }}</a></li>
-                                                @foreach ($category->subCategories as $subCategory)
+                                                @foreach($category->subCategories as $subCategory)
                                                     <li>
-                                                        <a href="#"
-                                                            class="sub-category-link">{{ $subCategory->name }}</a>
+                                                        <a href="#" class="sub-category-link">{{ $subCategory->name }}</a>
                                                     </li>
                                                 @endforeach
                                             </ul>
@@ -42,7 +38,7 @@
                                     </li>
                                 </ul>
                             </li>
-
+                            
                             <li class="dropdown "><a href="#">Blogs <i class="pe-7s-angle-down"></i></a>
                                 <ul class="sub-menu">
                                     <li><a href="blog-grid.html">Blog Grid Page</a></li>
@@ -62,53 +58,47 @@
                 <div class="col col-lg-auto align-self-center pl-0">
                     <div class="header-actions">
                         @auth
-                            @if (auth()->check() && auth()->user()->role_as == '0')
-                                <div class="dropdown">
-                                    <a href="#" role="button" id="accountDropdown" data-bs-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="false">
-                                        {{ auth()->user()->name }} <i class="pe-7s-angle-down"></i>
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="accountDropdown">
-                                        <li><a class="dropdown-item" href="#">Account</a></li>
-                                        <li>
-                                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault();
+                        @if (auth()->check() && auth()->user()->role_as == '0')
+                        <div class="dropdown">
+                            <a href="#" role="button" id="accountDropdown" data-bs-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                {{ auth()->user()->name }} <i class="pe-7s-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="accountDropdown">
+                                <li><a class="dropdown-item" href="#">Account</a></li>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">Logout</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            @endif
+                                </li>
+                            </ul>
+                        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    @endif
+                    
                         @else
                             <a href="login.html" class="header-action-btn login-btn" data-bs-toggle="modal"
                                 data-bs-target="#loginActive">Sign In</a>
                         @endauth
 
-
+                
                         <!-- Single Wedge Start -->
-                        <a href="#" class="header-action-btn" data-bs-toggle="modal"
-                            data-bs-target="#searchActive">
+                        <a href="#" class="header-action-btn" data-bs-toggle="modal" data-bs-target="#searchActive">
                             <i class="pe-7s-search"></i>
                         </a>
                         <!-- Single Wedge End -->
-
-                        @auth
-                            @if (auth()->user()->role_as == '0')
-                                <a href="{{ route('cart.show') }}" class="header-action-btn header-action-btn-cart pr-0">
-                                    <i class="pe-7s-shopbag"></i>
-                                </a>
-                            @endif
-                        @endauth
-
-                        <a href="#offcanvas-mobile-menu"
-                            class="header-action-btn header-action-btn-menu offcanvas-toggle d-lg-none">
+                        <a href="#offcanvas-cart" class="header-action-btn header-action-btn-cart pr-0">
+                            <i class="pe-7s-shopbag"></i>
+                            <span class="header-action-num">01</span>
+                            <!-- <span class="cart-amount">€30.00</span> -->
+                        </a>
+                        <a href="#offcanvas-mobile-menu" class="header-action-btn header-action-btn-menu offcanvas-toggle d-lg-none">
                             <i class="pe-7s-menu"></i>
                         </a>
                     </div>
                     <!-- Header Action End -->
-                </div>
+                </div>                
             </div>
         </div>
 </header>
@@ -120,39 +110,72 @@
 
         <div class="offcanvas-menu mb-4">
             <ul>
-                <li><a href="#">Home</a></li>
-
-                <li class="dropdown position-static">
-                    <a href="#">Shop <i class="pe-7s-angle-down"></i></a>
-                    <ul class="mega-menu d-block">
-                        <li class="d-flex">
-                            <ul class="d-block">
-                                <div class="image-wrapper mt-4">
-                                    <img src="{{ asset('images/banner/Shop_Image.jpg') }}" alt="Shop Image"
-                                        width="250px" height="auto" class="shop-image">
-                                    @foreach ($categories as $category)
-                                        @foreach ($category->subCategories as $subCategory)
-                                            <img src="{{ asset('storage/' . $subCategory->image) }}"
-                                                alt="{{ $subCategory->name }} Image" width="250px" height="auto"
-                                                class="sub-category-image">
-                                        @endforeach
-                                    @endforeach
-                                </div>
+                <li><a href="#"><span class="menu-text">Home</span></a>
+                    <ul class="sub-menu">
+                        <li><a href="index.html"><span class="menu-text">Home 1</span></a></li>
+                        <li><a href="index-2.html"><span class="menu-text">Home 2</span></a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><span class="menu-text">Shop</span></a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="#"><span class="menu-text">Shop Page</span></a>
+                            <ul class="sub-menu">
+                                <li class="title"><a href="#">Shop Page</a></li>
+                                <li><a href="shop-3-column.html">Shop 3 Column</a></li>
+                                <li><a href="shop-4-column.html">Shop 4 Column</a></li>
+                                <li><a href="shop-left-sidebar.html">Shop Left Sidebar</a></li>
+                                <li><a href="shop-right-sidebar.html">Shop Right Sidebar</a></li>
+                                <li><a href="shop-list-left-sidebar.html">Shop List Left Sidebar</a></li>
+                                <li><a href="shop-list-right-sidebar.html">Shop List Right Sidebar</a></li>
                             </ul>
-                            @foreach ($categories as $category)
-                                <ul class="d-block">
-                                    <li class="title"><a href="#">{{ $category->name }}</a></li>
-                                    @foreach ($category->subCategories as $subCategory)
-                                        <li>
-                                            <a href="#" class="sub-category-link">{{ $subCategory->name }}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            @endforeach
+                        </li>
+                        <li>
+                            <a href="#"><span class="menu-text">product Details Page</span></a>
+                            <ul class="sub-menu">
+                                <li><a href="single-product.html">Product Single</a></li>
+                                <li><a href="single-product-variable.html">Product Variable</a></li>
+                                <li><a href="single-product-affiliate.html">Product Affiliate</a></li>
+                                <li><a href="single-product-group.html">Product Group</a></li>
+                                <li><a href="single-product-tabstyle-2.html">Product Tab 2</a></li>
+                                <li><a href="single-product-tabstyle-3.html">Product Tab 3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span class="menu-text">Single Product Page</span></a>
+                            <ul class="sub-menu">
+                                <li><a href="single-product-slider.html">Product Slider</a></li>
+                                <li><a href="single-product-gallery-left.html">Product Gallery Left</a>
+                                </li>
+                                <li><a href="single-product-gallery-right.html">Product Gallery Right</a>
+                                </li>
+                                <li><a href="single-product-sticky-left.html">Product Sticky Left</a></li>
+                                <li><a href="single-product-sticky-right.html">Product Sticky Right</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span class="menu-text">Other Shop Pages</span></a>
+                            <ul class="sub-menu">
+                                <li><a href="cart.html">Cart Page</a></li>
+                                <li><a href="checkout.html">Checkout Page</a></li>
+                                <li><a href="compare.html">Compare Page</a></li>
+                                <li><a href="wishlist.html">Wishlist Page</a></li>
+                                <li><a href="my-account.html">Account Page</a></li>
+                                <li><a href="login.html">Login & Register Page</a></li>
+                                <li><a href="empty-cart.html">Empty Cart Page</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#"><span class="menu-text">Pages</span></a>
+                            <ul class="sub-menu">
+                                <li><a href="404.html">404 Page</a></li>
+                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                <li><a href="faq.html">Faq Page</a></li>
+                                <li><a href="coming-soon.html">Coming Soon Page</a></li>
+                            </ul>
                         </li>
                     </ul>
                 </li>
-
                 <li><a href="#"><span class="menu-text">Blog</span></a>
                     <ul class="sub-menu">
                         <li><a href="blog-grid.html">Blog Grid Page</a></li>
