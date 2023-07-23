c0836b7e6dda0c613b8368abc5ab661456f5812c
Thamalpath <BixSoftOfficial.com>
2023-07-23 21:48:48 +0530

    Added Product Remove Button in Cart Function

--- resources/views/layouts/web2.blade.php
+++ resources/views/layouts/web2.blade.php
@@ -99,6 +99,31 @@
                     },
                 });
             });
+
+
+            // Cart Remove Button
+            $(".delete-cart-item").click(function(e) {
+                e.preventDefault();
+
+                $.ajaxSetup({
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                    },
+                });
+
+                var prod_id = $(this).closest(".product_data").find(".prod_id").val();
+                $.ajax({
+                    method: "POST",
+                    url: "delete-cart-item",
+                    data: {
+                        prod_id: prod_id,
+                    },
+                    success: function(response) {
+                        window.location.reload();
+                        notyf.success(response.status);
+                    },
+                });
+            });
         });
     </script>
 
