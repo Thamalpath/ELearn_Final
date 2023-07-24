49223e5d5a3162edc11e29b1becce5daa27bde17
Thamalpath <BixSoftOfficial.com>
2023-07-24 14:41:55 +0530

    Added Checkout URL

--- resources/views/web/cart/cart.blade.php
+++ resources/views/web/cart/cart.blade.php
@@ -121,7 +121,7 @@
                                     </ul>
                                 </div>
                                 <h4 class="grand-totall-title">Grand Total <span>Rs.{{ $total }}.00</span></h4>
-                                <a href="#">Proceed to Checkout</a>
+                                <a href="{{ url('checkout') }}">Proceed to Checkout</a>
                             </div>
                         </div>
                     </div>
