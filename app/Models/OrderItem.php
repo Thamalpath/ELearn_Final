53dbb8a1650cfa3f247dd21eeac9a3ba5516ffc5
Thamalpath <BixSoftOfficial.com>
2023-07-26 18:54:41 +0530

    Relationship

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class OrderItem extends Model
 {
@@ -16,4 +17,9 @@ class OrderItem extends Model
         'price',
         'qty',
     ];
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class, 'prod_id');
+    }
 }
