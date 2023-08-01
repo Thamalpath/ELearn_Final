6644b38909f52bb4980ac6e26f068483a01166f4
Thamalpath <BixSoftOfficial.com>
2023-08-01 10:41:59 +0530

    Column Modified

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,97 +1,76 @@
 <x-app-layout>
     @section('content')
         <div class="m-3">
-            <table id="products" class="table table-bordered table-responsive table-striped dataTable dtr-inline" >
+            <table id="products" class="table table-bordered table-responsive table-striped dataTable dtr-inline">
                 <thead>
                     <tr>
                         <th>Product_ID</th>
-                        <th>Cat_Name</th>
-                        <th>Sub_Cat_Name</th>
+                        <th>Cat_Name & Sub_Cat_Name</th>
                         <th>Name</th>
                         <th>Description</th>
-                        <th>Original Price</th>
                         <th>Selling Price</th>
-                        <th>Tax</th>
                         <th>Image</th>
                         <th>Quantity</th>
-                        <th>Material</th>
-                        <th>Size</th>
-                        <th>Color</th>
                         <th>Status</th>
-                        <th>Trending</th>
-                        <th>Popular</th>
-                        <th>Meta Title</th>
-                        <th>Meta Description</th>
-                        <th>Meta Keywords</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($products as $product)
                         <tr>
-                            <td>{{$product->number}}</td>
-                            <td>{{$product->category_name}}</td>
-                            <td>{{$product->sub_category_name}}</td>
-                            <td>{{$product->name}}</td>
-                            <td>{{$product->description}}</td>
-                            <td>{{$product->original_price}}</td>
-                            <td>{{$product->selling_price}}</td>
-                            <td>{{$product->tax}}</td>
-                            <td>
-                                @foreach ((array) json_decode($product->images) as $image)
-                                    <img src="{{ asset('storage/'.$image) }}" alt="Image" height="100px" width="auto" class="mb-2">
-                                @endforeach
+                            <td>{{ $product->number }}</td>
+                            <td>{{ $product->category_name }} <br> & <br> {{ $product->sub_category_name }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ strlen($product->description) > 200 ? substr($product->description, 0, 150) . '...' : $product->description }}
                             </td>
-                            <td>{{$product->qty}}</td>
-                            <td>{{$product->material}}</td>
-                            <td>{{$product->size}}</td>
-                            <td>{{$product->color}}</td>
-                            <td>{{$product->status}}</td>
-                            <td>{{$product->trending}}</td>
-                            <td>{{$product->popular}}</td>
-                            <td>{{$product->meta_title}}</td>
-                            <td>{{$product->meta_description}}</td>
-                            <td>{{$product->meta_keywords}}</td>
+                            <td>{{ $product->selling_price }}</td>
+                            <td><img src="{{ asset('storage/' . json_decode($product->images)[0]) }}"
+                                    alt="{{ $product->name }}" height="100px" width="auto" />
+                            </td>
+                            <td>{{ $product->qty }}</td>
+                            <td>{{ $product->status }}</td>
                             <td>
-                                <a href="{{ route('product.edit', $product->id) }}" class="btn btn-primary mb-2">Edit</a> 
-                                
-                                <button type="button" class="btn btn-danger delete-btn" data-product_id="{{ $product->id }}" data-bs-toggle="modal" data-bs-target="#deleteproductModal">
+                                <a href="{{ route('product.edit', $product->id) }}" class="btn btn-primary mb-2">Edit</a>
+
+                                <button type="button" class="btn btn-danger delete-btn"
+                                    data-product_id="{{ $product->id }}" data-bs-toggle="modal"
+                                    data-bs-target="#deleteproductModal">
                                     Delete
-                                </button>                                
+                                </button>
                             </td>
                     @endforeach
                 <tbody>
             </table>
         </div>
-        
-<!-- Modal -->
-<div class="modal fade" id="deleteproductModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="deleteproductModal">Delete Product</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <form method="post" id="deleteForm">
-            @csrf
-            @method('DELETE')
-        <div class="modal-body">
-            Are you sure?
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Confirm</button>
-        </div>
-        </form>
-      </div>
-    </div>
-  </div>
 
+        <!-- Modal -->
+        <div class="modal fade" id="deleteproductModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deleteproductModal">Delete Product</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form method="post" id="deleteForm">
+                        @csrf
+                        @method('DELETE')
+                        <div class="modal-body">
+                            Are you sure?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Confirm</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     @endsection
     @section('scripts')
         <script>
             let table = new DataTable('#products');
-            $('.delete-btn').on('click', function(){
+            $('.delete-btn').on('click', function() {
                 let product_id = $(this).data('product_id');
                 $('#deleteForm').attr('action', '/product/' + product_id + '/delete');
             });
--- resources/views/admin/sub_category/index.blade.php
+++ resources/views/admin/sub_category/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     @section('content')
         <div class="m-3">
-            <table id="sub_categories" class="table table-bordered table-striped dataTable dtr-inline" >
+            <table id="sub_categories" class="table table-bordered table-striped dataTable dtr-inline">
                 <thead>
                     <tr>
                         <th>Sub_Cat_ID</th>
@@ -11,65 +11,65 @@
                         <th>Status</th>
                         <th>Image</th>
                         <th>Meta Title</th>
-                        <th>Meta Description</th>
-                        <th>Meta Keywords</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($sub_categories as $sub_category)
                         <tr>
-                            <td>{{$sub_category->number}}</td>
-                            <td>{{$sub_category->category_name}}</td>
-                            <td>{{$sub_category->name}}</td>
-                            <td>{{$sub_category->description}}</td>
-                            <td>{{$sub_category->status}}</td>
+                            <td>{{ $sub_category->number }}</td>
+                            <td>{{ $sub_category->category_name }}</td>
+                            <td>{{ $sub_category->name }}</td>
+                            <td>{{ $sub_category->description }}</td>
+                            <td>{{ $sub_category->status }}</td>
                             <td>
-                                <img src="{{ asset('storage/'.$sub_category->image) }}" alt="Image" height="125px" width="90px">
+                                <img src="{{ asset('storage/' . $sub_category->image) }}" alt="Image" height="125px"
+                                    width="90px">
                             </td>
-                            <td>{{$sub_category->meta_title}}</td>
-                            <td>{{$sub_category->meta_description}}</td>
-                            <td>{{$sub_category->meta_keywords}}</td>
+                            <td>{{ $sub_category->meta_title }}</td>
                             <td>
-                                <a href="{{ route('sub_category.edit', $sub_category->id) }}" class="btn btn-primary mb-2">Edit</a> 
-                                
-                                 <button type="button" class="btn btn-danger delete-btn"  data-sub_category_id={{$sub_category->id}} data-bs-toggle="modal" data-bs-target="#deletesub_categoryModal">
+                                <a href="{{ route('sub_category.edit', $sub_category->id) }}"
+                                    class="btn btn-primary mr-2">Edit</a>
+
+                                <button type="button" class="btn btn-danger delete-btn"
+                                    data-sub_category_id={{ $sub_category->id }} data-bs-toggle="modal"
+                                    data-bs-target="#deletesub_categoryModal">
                                     Delete
-                                </button> 
+                                </button>
                             </td>
                     @endforeach
                 <tbody>
             </table>
         </div>
-        
-<!-- Modal -->
-<div class="modal fade" id="deletesub_categoryModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="deletesub_categoryModal">Delete Sub Category</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <form method="post" id="deleteForm">
-            @csrf
-            @method('DELETE')
-        <div class="modal-body">
-            Are you sure?
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Confirm</button>
-        </div>
-        </form>
-      </div>
-    </div>
-  </div>
 
+        <!-- Modal -->
+        <div class="modal fade" id="deletesub_categoryModal" data-bs-backdrop="static" data-bs-keyboard="false"
+            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deletesub_categoryModal">Delete Sub Category</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form method="post" id="deleteForm">
+                        @csrf
+                        @method('DELETE')
+                        <div class="modal-body">
+                            Are you sure?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Confirm</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     @endsection
     @section('scripts')
         <script>
             let table = new DataTable('#sub_categories');
-            $('.delete-btn').on('click', function(){
+            $('.delete-btn').on('click', function() {
                 let sub_category_id = $(this).data('sub_category_id');
                 $('#deleteForm').attr('action', '/sub_category/' + sub_category_id + '/delete');
             });
