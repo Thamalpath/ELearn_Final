aa6c5c213e45e105ece8f3549e758888c8547c1e
Thamalpath <BixSoftOfficial.com>
2023-08-11 13:20:19 +0530

    Sidebar Logo Issue Fixed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="images/logo/Logo.png" alt="Daisy Wardrobe Logo" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('images/logo/Logo.png') }}" alt="Daisy Wardrobe Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
